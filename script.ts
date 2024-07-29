0c834aa77067a7dfc2cb7ea45b02d5e80766fab4
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-07-29 12:37:50 +0530

    some other type like unknown , never, any

--- script.ts
+++ script.ts
@@ -1,14 +1,41 @@
-// types
-// let a;
-// in ts don't use any variable type
-
-let a : number;
-let arr : [];
-let s: string = "this is a string";
-let s2 = "this is another string";
-// if we set value at the declaration time then we dont have to write : string
-
-// tuples
-// when we declear at first that the first value is boolean, number and string then it is a touple
-
 let arr2: [boolean, number, string] = [true, 1, "avijit"];
+
+arr2.push(12);
+// this create a loop and this is not show any error.
+
+let a : any;
+a = 12;
+a = "23";
+
+let b: unknown;
+// when we did not sure what is the type of this variable. it is similar to any but have some difference.
+
+
+// another type is never which is used for fucntion which did not return anything
+
+const abcd = (): never => {
+    while(true){
+        console.log("running");
+    }
+}
+
+// function abcd(): never{
+//     while(true){
+//         console.log("running");
+//     }
+// }
+
+abcd();
+// after the never type code didnot run
+console.log("this is the end");
+// this console.log did not run because of never type.
+
+const sum = () : number => {
+    return 1;
+}
+sum();
+
+const ab = () : void => {
+    console.log("hey");
+}
+ab();
