b7766e60e0dac8d286cae69ed0e922043123dd48
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-07-29 21:40:58 +0530

    intersection types

--- script.ts
+++ script.ts
@@ -1,29 +1,24 @@
-// Type inference
+// intersection types
 
-let a = 12;
-// here we did not declear for a that it is a number but it automatically set it to see the value. This is called type inference.
+let a: number & string;
+// so here it shows the the type of a is never
 
-let variable: string | null;
-variable = null;
-variable = "hello";
-// so here we can decleared the type of the variable is string or null;
 
-let c: string | number;
-c = "12";
-// now if we write c.  then it show us all the functions which is common among string and number. But if we want to see all the functions for string then-
-
-if(typeof c === 'string'){
-    // c.
-}else if(typeof c=== 'number'){
-    // c. so here it didnot shows all the number's methods because here clearly it find 'c' is a stirng but if werite it for a function then it work.
+type City = {
+    name: string,
+    population: number,
 }
 
-const anyFunction = (val : string | number): void => {
-    if(typeof val === 'string'){
-        // val. here it shows all the methods for stirng
-    }else if(typeof val === 'number'){
-        // val. here it shows all the methods for number
-    }
+type Planet = {
+    name: string,
+    cities: number,
 }
 
-anyFunction(c);
+type citiesInPlanet = City & Planet;
+let value: citiesInPlanet = {
+    name: "Kolkata",
+    population: 10000,
+    cities: 12
+}
+
+// if one of the value was missing then it shows an error.
