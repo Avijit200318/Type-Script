2b7049b77f41acd3dec0d4d9040861aa12c7508f
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-07-29 12:21:44 +0530

    added my first typescript code

---
+++ script.ts
@@ -0,0 +1,14 @@
+// types
+// let a;
+// in ts don't use any variable type
+
+let a : number;
+let arr : [];
+let s: string = "this is a string";
+let s2 = "this is another string";
+// if we set value at the declaration time then we dont have to write : string
+
+// tuples
+// when we declear at first that the first value is boolean, number and string then it is a touple
+
+let arr2: [boolean, number, string] = [true, 1, "avijit"];
