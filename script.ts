578c5f9f523b4d9faed1a1ccbbdca08f12a34758
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-07-29 12:47:50 +0530

    enums in ts

--- script.ts
+++ script.ts
@@ -1,41 +1,11 @@
-let arr2: [boolean, number, string] = [true, 1, "avijit"];
-
-arr2.push(12);
-// this create a loop and this is not show any error.
-
-let a : any;
-a = 12;
-a = "23";
-
-let b: unknown;
-// when we did not sure what is the type of this variable. it is similar to any but have some difference.
-
-
-// another type is never which is used for fucntion which did not return anything
-
-const abcd = (): never => {
-    while(true){
-        console.log("running");
-    }
+// Enums
+// enums help us to group things
+enum direction {
+    bottom = "bottom",
+    left = "left",
+    right = 'right',
+    top = "top"
 }
 
-// function abcd(): never{
-//     while(true){
-//         console.log("running");
-//     }
-// }
-
-abcd();
-// after the never type code didnot run
-console.log("this is the end");
-// this console.log did not run because of never type.
-
-const sum = () : number => {
-    return 1;
-}
-sum();
-
-const ab = () : void => {
-    console.log("hey");
-}
-ab();
+direction.top;
+// then we get this values. its like the object but instead of : here = used.
